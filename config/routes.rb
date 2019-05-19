8cc70d41046ca6be03a370c0818e2a161f543553
olaid <olaid@hotmail.co.jp>
2019-05-19 13:57:41 +0900

    initialize repository

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   root 'main_pages#home'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  namespace :api, format: 'json' do
+   resources :tasks, only: [:index, :create, :update]
+  end
 end
